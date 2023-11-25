2176069e4ad343364f8c2a3cff352da81d2b0178
anastar5858 <anas.tarrab58@gmail.com>
2023-11-25 01:07:17 +0000

    ..

--- assets/js/forward-demo-service/fetchDemo.js
+++ assets/js/forward-demo-service/fetchDemo.js
@@ -1,7 +1,10 @@
 const fetchAllDemos = async (setListOfDemo) => {
     const urlWithoutSearchParams = window.location.origin + window.location.pathname;
     // fetch the demos of this page only
-    const demosResponse = await fetch(`https://rms-back-90595d39ec60.herokuapp.com/api/fetch-demos/${encodeURIComponent(urlWithoutSearchParams)}`);
+    const demosResponse = await fetch(`https://rms-back-90595d39ec60.herokuapp.com/api/fetch-demos/${encodeURIComponent(urlWithoutSearchParams)}`, {
+        method: 'GET',
+        credentials: 'include',
+    });
     if (demosResponse.ok) {
         const data = await demosResponse.json();
         console.log(data)
--- assets/js/forward-demo-service/fetchDemoDomain.js
+++ assets/js/forward-demo-service/fetchDemoDomain.js
@@ -1,6 +1,9 @@
 const getDemosOfDomain = async (domain, setAllDemosDomain) => {
     // fetch the demos of this page only
-    const demosResponse = await fetch(`https://rms-back-90595d39ec60.herokuapp.com/api/fetch-demos-domain/${domain}`);
+    const demosResponse = await fetch(`https://rms-back-90595d39ec60.herokuapp.com/api/fetch-demos-domain/${domain}`, {
+        method: 'GET',
+        credentials: 'include',
+    });
     if (demosResponse.ok) {
         const data = await demosResponse.json();
         setAllDemosDomain(data);
